14c294087a5cc2e182c134288f899df48fce7ca0
morgana <89588301+morgana-x@users.noreply.github.com>
2025-02-11 16:50:55 +1100

    Add files via upload

---
+++ IDXLib/IDXFile.cs
@@ -0,0 +1,15 @@
+﻿namespace IDXLib
+{
+    public class IDXFile
+    {
+        public long Location;
+        public ulong Size;
+        public string Name;
+        public IDXFile(string Name, long Location, ulong Size)
+        {
+            this.Name = Name;
+            this.Location = Location;
+            this.Size = Size;
+        }
+    }
+}
---
+++ IDXLib/Program.cs
@@ -0,0 +1,20 @@
+﻿using IDXLib;
+public partial class Program
+{
+    public static void Main (string[] args)
+    {
+        string filePath = "";
+        if (args.Length > 0 )
+            filePath = args[0];
+        else
+        {
+            Console.WriteLine("Drag and drop IDX file to extract!");
+            filePath = Console.ReadLine().Replace("\"","");
+        }
+        IDX idx = new IDX(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+        string folder = filePath.Replace(".idx", "");
+        if (!Directory.Exists(folder))
+            Directory.CreateDirectory(folder);
+        idx.ExtractAll(folder);
+    }
+}
